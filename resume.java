9dc865931838818e915c366e5e7dc08f366dc040
bharathreo29 <62152862+bharathreo29@users.noreply.github.com>
2021-01-02 23:09:59 +0530

    Create resume.java
    hacker rank color pair identification code

---
+++ resume.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void findPairs()
+    {
+        
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s = new Scanner(System.in);
+        int[] arr;
+        String srtArr; 
+        int n = 0;
+        int i = 0;
+        try
+        {
+            n = s.nextInt();
+            srtArr = s.nextLine();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        arr = new int[n];
+        System.out.println(srtArr);
+        System.out.println("Worked");
+	}
+}
